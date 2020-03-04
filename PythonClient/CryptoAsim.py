be74151d260ff2fadbe971bd7172648d7816143f
Florian Dubath <florian.dubath@unige.ch>
2020-03-04 14:08:11 +0100

    New version tested to be compatible with the JS

--- PythonClient/CryptoAsim.py
+++ PythonClient/CryptoAsim.py
@@ -1,14 +1,8 @@
-from web3 import Web3
-from web3.eth import Eth
-from eth_account import Account
-from Crypto import Random
 from Crypto.Cipher import AES
-from Crypto.PublicKey import ECC
 from Crypto.Hash import SHA256, SHA512
+from ecdsa import SigningKey, SECP256k1, ECDH, VerifyingKey
 import struct
-import base64
 import hmac
-import hashlib
 
 
 """"
@@ -34,36 +28,27 @@ def AES256CbcEncrypt(bin_data, key=b'0'*32, iv=b'0'*16):
     aes = AES.new(key, AES.MODE_CBC, iv)
     return aes.encrypt(pad(bin_data))
     
-def encode_key(pub_key):
-    return pub_key.pointQ.x.to_bytes(32)+ pub_key.pointQ.y.to_bytes(32)  
-    
-def decode_key(encoded_pub_key):
-    x = encoded_pub_key[:32]
-    y = encoded_pub_key[32:]
-    return ECC.EccPoint(int.from_bytes(x,byteorder='big'), int.from_bytes(y,byteorder='big')) 
-    
-def derive(private_key, pub_ecc_point):
-    return (pub_ecc_point*private_key.d).x  
-    
-    
 def EncryptMessage(publicKey_hex, plainText_string):
     if publicKey_hex[:2]=='0x':
         publicKey_hex=publicKey_hex[2:]
+    publicKey_bin=bytearray.fromhex(publicKey_hex)
     
     # Generate the temporary key 
-    ephemPrivKey = ECC.generate(curve='secp256r1')
-    ephemPubKey = ephemPrivKey.public_key()
-    ephemPubKeyEncoded = encode_key(ephemPubKey)
+    ecdh = ECDH(curve=SECP256k1)
+    ecdh.generate_private_key()
+    
+    ephemPubKeyEncoded = bytearray.fromhex(ecdh.get_public_key().to_string().hex()) 
     
     # Load the public key
-    publicKey = bytearray.fromhex(publicKey_hex)
-    pub = ECC.EccPoint(int.from_bytes(publicKey[:int(len(publicKey)/2)],byteorder='big'), int.from_bytes(publicKey[int(len(publicKey)/2):],byteorder='big'))
-    
+    publicKey = VerifyingKey.from_string(publicKey_bin, curve=SECP256k1)
+     
     # ECDH => get the shared secret
-    px = derive(ephemPrivKey,pub) 
-    
+    ecdh.load_received_public_key(publicKey)
+        
+    px = ecdh.generate_sharedsecret_bytes()
+
     # compute the encription and MAC keys
-    hash_px = SHA512.new(data=px.to_bytes()).digest()
+    hash_px = SHA512.new(data=px).digest()
     encryptionKey = hash_px[:32]
     macKey = hash_px[32:]
     
@@ -73,44 +58,47 @@ def EncryptMessage(publicKey_hex, plainText_string):
     ciphertext = AES256CbcEncrypt(plaintext,encryptionKey,iv)
     
     # compute the MAC
-    dataToMac = iv + ephemPubKeyEncoded + ciphertext
+    dataToMac = iv + bytearray([4])+ephemPubKeyEncoded + ciphertext
     mac = hmac.new(macKey, dataToMac, 'sha256').digest()
     
     #build the output
-    serializedCiphertext = iv + ephemPubKeyEncoded + mac + ciphertext
+    serializedCiphertext = iv + bytearray([4])+ephemPubKeyEncoded + mac + ciphertext
     return serializedCiphertext.hex()
     
     
     
-def DecryptMessage(privateKey_hey, encrypted_hex):
-    if privateKey_hey[:2]=='0x':
-        privateKey_hey=privateKey_hey[2:]
+def DecryptMessage(privateKey_hex, encrypted_hex):
+    if privateKey_hex[:2]=='0x':
+        privateKey_hex=privateKey_hex[2:]
     if encrypted_hex[:2]=='0x':
         encrypted_hex=encrypted_hex[2:]
 
     # get the components 
     encrypted = bytearray.fromhex(encrypted_hex)
     iv = encrypted[:16]
-    ephemPubKeyEncoded = encrypted[16:80]
-    mac = encrypted[80:112]
-    ciphertext = encrypted[112:]
+    ephemPubKeyEncoded = encrypted[17:81]
+    mac = encrypted[81:113]
+    ciphertext = encrypted[113:]
     
     # recover the temporary public key
-    ephemPubKey = decode_key(ephemPubKeyEncoded)
+    ephemPubKey = VerifyingKey.from_string(ephemPubKeyEncoded, curve=SECP256k1)
     
     # load the private key
-    privKey = ECC.construct(curve='secp256r1', d=int(privateKey_hey,16))
+    priv_key = SigningKey.from_secret_exponent(int(privateKey_hex,16), curve=SECP256k1)
+    ecdh = ECDH(curve=SECP256k1, private_key=priv_key) 
     
     # ECDH => get the shared secret
-    px = derive(privKey, ephemPubKey)
+    ecdh.load_received_public_key(ephemPubKey)
+    px = ecdh.generate_sharedsecret_bytes()
+
     
     # compute the encription and MAC keys
-    hash_px = SHA512.new(data=px.to_bytes()).digest()
+    hash_px = SHA512.new(data=px).digest()
     encryptionKey = hash_px[:32]
     macKey = hash_px[32:]
     
     # check the MAC
-    dataToMac = iv + ephemPubKeyEncoded + ciphertext
+    dataToMac = iv + bytearray([4])+ephemPubKeyEncoded + ciphertext
     computed_mac = hmac.new(macKey, dataToMac, 'sha256').digest()
     if computed_mac!=mac:
         raise ValueError("MAC missmatch")
@@ -118,3 +106,4 @@ def DecryptMessage(privateKey_hey, encrypted_hex):
     #decipher the text
     plaintext = AES256CbcDecrypt(ciphertext.hex(), encryptionKey, iv)
     return plaintext.decode("utf-8")
+    
