252daedd1ccf94db46aa323b53e8d0bee2f32930
Valentin Lab <valentin.lab@kalysto.org>
2024-04-03 18:11:05 +0200

    new: pkg: make package compatible with python ``3.11``

--- src/pyc3l/__init__.py
+++ src/pyc3l/__init__.py
@@ -4,6 +4,29 @@ import logging
 import time
 import codecs
 
+
+## Monkey-patching parsimonious 0.8 to support Python 3.11
+
+import sys
+import inspect
+
+# Check if we're running on Python 3.11 or later
+if sys.version_info >= (3, 11):
+    # Implement a getargspec function using getfullargspec for compatibility
+    def getargspec(func):
+        full_argspec = inspect.getfullargspec(func)
+        args = full_argspec.args
+        varargs = full_argspec.varargs
+        varkw = full_argspec.varkw
+        defaults = full_argspec.defaults
+        return inspect.ArgSpec(args, varargs, varkw, defaults)
+
+    # Monkey patch the inspect module
+    inspect.getargspec = getargspec
+
+## End of monkey-patching parsimonious
+
+
 from web3 import Web3
 from web3.eth import Eth
 import eth_abi
